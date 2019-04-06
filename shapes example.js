fa442c042000eaf7f2683aec6e6e099f80e19573
Anat Fradin <anat.fradin@google.com>
2019-04-06 20:08:07 +0300

    first commit

---
+++ shapes example.js
@@ -0,0 +1,20 @@
+var Shapes;
+(function (Shapes) {
+    var Point = /** @class */ (function () {
+        function Point(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        Point.prototype.getDist = function () {
+            return Math.sqrt(this.x * this.x + this.y * this.y);
+        };
+        Point.origin = new Point(0, 0);
+        return Point;
+    }());
+    Shapes.Point = Point;
+})(Shapes || (Shapes = {}));
+//Local variables
+var p = new Shapes.Point(3, 4);
+var dist = p.getDist();
+console.log("dist = " + dist);
+//# sourceMappingURL=shapes example.js.map
